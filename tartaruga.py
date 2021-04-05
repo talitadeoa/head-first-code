f13680412d9553a1240624d93718c1feaeadcb88
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-04-05 16:47:23 -0300

    Update tartaruga.py

--- tartaruga.py
+++ tartaruga.py
@@ -1,1 +1,10 @@
 import turtle 
+
+tortuguinha = turtle.Turtle()
+tortuguinha.shape('turtle')
+
+for i in range (0,4):
+    tortuguinha.forward(100)
+    tortuguinha.right(90)
+
+    turtle.mainloop()
