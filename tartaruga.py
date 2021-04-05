d6f1d75fa4ce41c94f5fbfb9a93268bc996ba043
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-04-05 17:11:36 -0300

    .

--- tartaruga.py
+++ tartaruga.py
@@ -3,21 +3,7 @@ import turtle
 tortuguinha = turtle.Turtle()
 tortuguinha.shape('turtle')
 tortuguinha.color('red')
-tortugo = turtle.Turtle()
-tortugo.shape('turtle')
-tortugo.color('blue')
 
-def faz_quadradin(the_turtle):
-
-    for i in range(0,4):
-        the_turtle.forward(100)
-        the_turtle.right(90)
-
-def faz_espiral(the_turtle):
-    for i in range(0,36):
-        faz_quadradin(the_turtle)
-        the_turtle.right(10)
-
-faz_espiral(tortuguinha)
-tortugo.right(5)
-faz_espiral(tortugo)
+for i in range(5):
+    tortuguinha.forward(100)
+    tortuguinha.right(144)
